07347a0e10b0121c63072d328a311acfc8de6321
Chan <wlsgud267@naver.com>
2026-06-08 13:59:22 +0900

    업로드를 깜박했네

--- game/NetworkConfig.h
+++ game/NetworkConfig.h
@@ -1,6 +1,6 @@
 #pragma once
 
 namespace NetworkConfig {
-    constexpr const char* ServerHost = "169.213.25.19";
+    constexpr const char* ServerHost = "168.107.46.161";
     constexpr unsigned short ServerPort = 9000;
 }
